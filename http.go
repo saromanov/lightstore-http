87beddb7c6de0ea603299d9365a408f0b1b13370
Sergey <xxsmotur@gmail.com>
2018-11-28 00:34:26 +0500

    Init of lightstore config

--- http.go
+++ http.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/saromanov/lightstore/store"
 )
 
 var port = flag.Int("port", 8080, "port")
+
+var ls *store.Lightstore
 
 func set(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Set new key")
@@ -18,6 +22,7 @@ func get(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	ls = store.Open(nil)
 	http.HandleFunc("/set", set)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
